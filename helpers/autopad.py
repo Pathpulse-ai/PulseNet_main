2be0f41642bf8fb3476b2313daf5888a6a0d7c93
ibrahimPathpulse2024 <ibrahim@pathpulse.ai>
2025-01-12 11:38:04 +0530

    changes in autopad

--- helpers/autopad.py
+++ helpers/autopad.py
@@ -1,10 +1,7 @@
-def autopad(k, p=None, d=1):
-    """
-    k: kernel size (int)
-    p: padding size (int) or None
-    d: dilation (int)
-    """
-    # If p is not provided, calculate 'same' padding for given kernel size and dilation
+def autopad(k, p=None, d=1):  # kernel, padding, dilation
+    # Pad to 'same' shape outputs
+    if d > 1:
+        k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]  # actual kernel-size
     if p is None:
-        p = (k - 1) * d // 2
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
     return p
